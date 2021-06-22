3529ea6eb01a77551192b544efd2b0f14f2c4e42
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-06-22 07:46:43 -0700

    Add the modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [modal, setModal] = useState(false);
+  const Toggle = () => setModal(!modal);
+
   return (
     <div className='App'>
-      <button>Click</button>
+      <button onClick={() => Toggle()}>Click</button>
+      {modal && <div className='modal'>This is a modal</div>}
     </div>
   );
 }
